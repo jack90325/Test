598cd577857f41016f2931da2bb7096716d95cd6
jack90325 <jack90325@gmail.com>
2022-04-20 20:37:25 +0800

    initial commit

---
+++ src/isPrime.js
@@ -0,0 +1,14 @@
+module.exports = {
+    isPrime(num) {
+        let t = parseInt(Math.sqrt(num));
+        for (let i = 2; i < t; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+  
